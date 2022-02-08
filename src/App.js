961679abc7f557e30d5d16c7320857f68d38d7a4
arun-me <aruninnovate@pm.me>
2022-02-08 08:32:02 +0530

    initize

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>font-awesome cdn</h1>
+      
+      <a href="https://cdnjs.com/libraries/font-awesome">https://cdnjs.com/libraries/font-awesome</a>
+      <i class="far fa-times-circle"></i>
+      <br/>
+      <a href="https://fontawesome.com/v5.15/icons?d=gallery&p=2&q=close&m=free">https://fontawesome.com/v5.15/icons?d=gallery&p=2&q=close&m=free</a>  <br/>
+      <a href="https://css-tricks.com/favicons-next-to-external-links/">https://css-tricks.com/favicons-next-to-external-links/</a>
+      <br/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
